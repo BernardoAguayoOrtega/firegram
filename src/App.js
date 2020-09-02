fa1f272a370b0ca1c2b649b13546fa8435ebc490
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-01 20:55:18 -0500

    create upload form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+//import react
 import React from 'react';
+//import title component
 import Title from './components/Title';
+//import Upload component
+import UploadForm from './components/UploadForm';
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <UploadForm/>
     </div>
   );
 }
---
+++ src/components/UploadForm.js
@@ -0,0 +1,13 @@
+//import react
+import React from 'react';
+
+//create and export upload Form
+const UploadForm = () => {
+	return (
+		<>
+			<input type='file' />
+		</>
+	);
+};
+
+export default UploadForm;

5e69aa960491beeec6c07cd2d7ae0da0202e6429
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-01 21:19:27 -0500

    create logic to check if the file that we resive is an image

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,12 +1,33 @@
 //import react
-import React from 'react';
+import React, { useState } from 'react';
 
 //create and export upload Form
 const UploadForm = () => {
+	//use state hook for file
+	const [file, setFile] = useState(null);
+	//use state hook for error
+	const [error, setError] = useState(null);
+	//array with allow types
+	const types = ['image/png', 'image/jpeg'];
+	//changeHandler
+	const changeHandler = (e) => {
+		const selected = e.target.files[0];
+
+		if (selected && types.includes(selected.type)) {
+			setFile(selected);
+		} else {
+			setFile(null);
+			setError('please select an image file (png or jpeg)');
+		}
+	};
+
 	return (
-		<>
-			<input type='file' />
-		</>
+		<form>
+			<input type='file' onChange={changeHandler} />
+			<div className='output'>
+				{error && <div className='error'>{error}</div>}
+			</div>
+		</form>
 	);
 };
 

765bc7c9b1192f28c21a61ddf6fd8726b39a20b0
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-01 20:30:57 -0500

    add firebase config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //import react
 import React from 'react';
 //import react dom
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 //import styles
 import './index.css';
 //import app component
@@ -9,13 +9,10 @@ import App from './App';
 //import hash router component
 import { HashRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
+//render react dom
+render(
 	<Router basename='/'>
 		<App />
 	</Router>,
 	document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
